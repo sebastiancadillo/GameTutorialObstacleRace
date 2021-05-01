54df33b74222435f184daae22d5785ec136885ac
sebastiancadillo <sebastiancadillo00@gmail.com>
2021-05-01 14:24:49 -0400

    ProjectUpdate

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    void OnCollissionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit an obstacle!");
+            movement.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log("Hello, World!");
+        // rb.useGravity = false; cancels gravity
+        // rb.AddForce(0, 200, 500); pushes cube
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if ( Input.GetKey("d") )
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
